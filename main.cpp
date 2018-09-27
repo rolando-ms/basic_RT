d044131adf3be1a029d6bc326932cbafcc996108
rolando-ms <rolixman3@hotmail.com>
2018-09-27 14:29:13 +0200

    Hit sphere function added. Plots a red 'sphere'

--- main.cpp
+++ main.cpp
@@ -4,7 +4,26 @@
 #include "vec3.h"
 #include "ray.h"
 
+
+// Function that checks if a ray is hitting a sphere according to
+// the solution of dot((p-c),(p-c)) = r^2 : p=o+t*d
+// -> t^2*dot(d,d) + 2*t*dot(t,o-c) + dot((o-c),(o-c)) = r^2
+// If discriminant > 0 -> 2 real solutions (ray hits the sphere twice)
+// if discriminant = 0 -> 1 real solution (ray hits sphere on surface)
+// if discriminant < 0 -> No real solutions (No hit)
+bool hit_sphere(const vec3& center, float radius, const ray& r){
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+    float discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
+
 vec3 color(const ray& r){
+    if (hit_sphere(vec3(0,0,-1), 0.5, r))
+        return vec3(1,0,0);
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y() + 1.0);
     // Linear interpolation
