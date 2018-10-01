9963bd552a7ed3c2082b8149da4a7f54b46391b6
rolando-ms <rolixman3@hotmail.com>
2018-10-01 22:07:38 +0200

    Camera class added. Main modified to take mean samples per pixel. Img not correct for >1 sample

---
+++ headers/camera.h
@@ -0,0 +1,26 @@
+//
+// Created by rolandoms on 01.10.18.
+//
+
+#ifndef SHIRLEY_CAMERA_H
+#define SHIRLEY_CAMERA_H
+
+#include "ray.h"
+
+class camera{
+public:
+    camera(){
+        lower_left_corner = vec3(-2.0, -1.0, -1.0);
+        horizontal = vec3(4.0, 0.0, 0.0);
+        vertical = vec3(0.0, 2.0, 0.0);
+        origin = vec3(0.0, 0.0, 0.0);
+    }
+    ray get_ray(float u, float v) {return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);}
+
+    vec3 origin;
+    vec3 lower_left_corner;
+    vec3 horizontal;
+    vec3 vertical;
+};
+
+#endif //SHIRLEY_CAMERA_H
--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "hitablelist.h"
 //#include "float.h"
 #include "values.h"
+#include "camera.h"
+//#include <stdlib.h>
 
 //// Function that checks if a ray is hitting a sphere according to
 //// the solution of dot((p-c),(p-c)) = r^2 : p=o+t*d
@@ -51,27 +53,28 @@ int main() {
     // Image Dimensions
     int nx = 200;
     int ny = 100;
+    int ns = 1;
     std::ofstream ofs;
     ofs.open("file.ppm");
     // PPM header: P6 = PPM format; width; height; 8bits
     ofs << "P6\n" << std::to_string(nx) << " " << std::to_string(ny) << "\n255\n";
     // Righ hand coordinate system (x+ = right; y+ = up; z+ = towards user)
-    vec3 lower_left_corner(-2.0,-1.0,-1.0);
-    vec3 horizontal(4.0,0.0,0.0);
-    vec3 vertical(0.0,2.0,0.0);
-    vec3 origin(0.0,0.0,0.0);
     hitable *list[2];
     list[0] = new sphere(vec3(0,0,-1),0.5);
     list[1] = new sphere(vec3(0,-100.5,-1),100);
     hitable *world = new hitable_list(list,2);
+    camera cam;
     for(int j = ny-1; j>=0; j--){
         for(int i = 0; i<nx; i++){
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-//            float b = 0.2;
-            vec3 p = r.point_at_parameter(2.0);
-            vec3 col = color(r,world);
+            vec3 col(0.0,0.0,0.0);//Color initialization
+            for(int s=0; s < ns; s++){
+                float u = float(i) / float(nx);
+                float v = float(j) / float(ny);
+                ray r = cam.get_ray(u,v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r,world);
+            }
+            col /= float(ns);// Mean of all samples around pixel (i,j)
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
